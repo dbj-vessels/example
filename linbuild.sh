be72050abba6a657b0026c3c5bf7ddf90c3a4670
dbj <dbj@dbj.org>
2023-06-13 15:15:16 +0000

    added

---
+++ linbuild.sh
@@ -0,0 +1,8 @@
+# (c) dbj@dbj.org CC BY SA 4.0
+#
+# Be sure to read and understand 
+# https://towardsdatascience.com/a-complete-guide-to-using-environment-variables-and-files-with-docker-and-compose-4549c21dc6af
+#
+docker-compose down --rmi all
+
+docker-compose up
