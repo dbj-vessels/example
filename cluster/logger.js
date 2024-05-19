03607838dea5367f326ad9c43e6c053e6c70564e
dbjdbj <dbj@dbj.org>
2024-05-19 14:59:07 +0200

    no double timestamp

--- cluster/logger.js
+++ cluster/logger.js
@@ -4,9 +4,9 @@ const winston = require('winston');
 const logger = winston.createLogger({
     level: 'info', // Set the log level (e.g., 'info', 'debug', 'warn', 'error')
     format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.printf(({ timestamp, level, message }) => {
-            return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+        /* winston.format.timestamp(),*/
+        winston.format.printf(({ /*timestamp,*/ level, message }) => {
+            return `[${level.toUpperCase()}]: ${message}`;
         })
     ),
     transports: [
